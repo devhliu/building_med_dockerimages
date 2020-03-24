27837ff506b5331b98226ab7db55d26525b8dc11
hliudev <hliudev@email.com>
2020-03-24 14:08:40 +0800

    add docker install clara

--- install_scripts_ubuntu/21_install_dockerce.sh
+++ install_scripts_ubuntu/21_install_dockerce.sh
@@ -36,4 +36,6 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 apt-get update && sudo apt-get install -y nvidia-container-toolkit
+# to run docker without sudo
+sudo usermod -a -G docker ubuntu # and then log out and back in
 systemctl restart docker
